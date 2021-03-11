ff9b9890c60436df58a4122a0ac67f2a7b5a5904
powerferren@gmail.com <powerferren@gmail.com>
2021-03-11 13:00:31 +0200

    Creating a list of companies names, websites and phones

--- parser.py
+++ parser.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup as bSoup
 import csv
 
 
-def write_csv(data):
+#TODO: Need to change writer for write Dictionary. Use csv.DictWriter()
+'''def write_csv(data):
     with open('companies.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow((data['name'], data['url']))
+        columns = ['name', 'url']
+        writer = csv.DictWriter(f, fieldnames=columns)
+        #writer.writeheader()
+        writer.writerow(data)
+'''
 
 
 def get_html(url):
@@ -17,20 +21,55 @@ def get_html(url):
 
 
 def get_page_data(html):
+    company_list = []
     soup = bSoup(html, 'lxml')
     companies = soup.find('ul', class_ = 'logotypes-squares').find_all('li')
     for company in companies:
         name = company.find('a').find('h5').text
         company_url = 'https://www.work.ua' + company.find('a').get('href')
         data = {'name': name, 'url': company_url}
-        print(data)
-        write_csv(data)
+        company_list.append(data)
+    return company_list
+
+
+def get_company_data(list_of_companies):
+    list_of_companies_data = []
+    for item in list_of_companies:
+        new_request = requests.get(item['url'])
+        if new_request.ok:
+            company_soup = bSoup(new_request.text, 'lxml')
+            name_of_company = company_soup.find('h1').text
+            website_of_company = company_soup.find('span', class_ = 'website-company')
+            if website_of_company:
+                website = website_of_company.find('a').get('href')
+            else:
+                website = None
+            glyphicon_phone = company_soup.find('span', class_ = 'glyphicon-phone')
+            if glyphicon_phone:
+                tel_contact = glyphicon_phone.find_parent('p')
+            else:
+                tel_contact = None
+            if tel_contact:
+                tel_a = tel_contact.find('a')
+            else:
+                tel_a = None
+            if tel_a:
+                tel = tel_a.text
+            else:
+                tel = None
+            data_company = {'name': name_of_company, 'website': website, 'phone': tel}
+            list_of_companies_data.append(data_company)
+            #print(data_company)
+    return list_of_companies_data
 
 
 def main():
     url = 'https://www.work.ua/ru/jobs/by-company/'
     html = get_html(url)
-    get_page_data(html)
+    all_list = get_page_data(html)
+    print_list = get_company_data(all_list)
+    print(print_list)
+
 
 
 if __name__ == '__main__':
